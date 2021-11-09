4c7f2b824ee9258598fd80abbed1de4a0c56d713
sergeyt4444 <sergeyt4444@rambler.ru>
2021-11-10 02:59:36 +0300

    small change in ch2ex9 main

--- src/com/mycompany/exercises/chapter2/exercise9/Main.java
+++ src/com/mycompany/exercises/chapter2/exercise9/Main.java
@@ -4,6 +4,7 @@ package com.mycompany.exercises.chapter2.exercise9;
 public class Main {
     public static void main(String[] args) {
         Car car = new Car(10, 30);
+        System.out.println(car);
         car.drive(100);
         System.out.println(car);
         car.refuel(1);
